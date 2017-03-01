f27686e7be94bdbc6b2525256b6abe576f2fc39d
AdeBagusSandry <adebagussandry@gmail.com>
2017-03-01 13:47:09 +0700

    Add files via upload

---
+++ Mahasiswa1.java
@@ -0,0 +1,13 @@
+public class Mahasiswa1{
+	public static void main(String[] args){
+
+System.out.print("Nama Lengkap :  " );
+System.out.println("Ade Bagus Sandry ");
+System.out.print("NIM          :         ");
+System.out.println("10114043");
+System.out.print("Alamat :");
+System.out.println("Jl. Dago Pojok No.28A");
+
+
+	}
+}
